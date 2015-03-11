eab0a2a395a4a02a7b693bc754c4f71cac4bff2f
Guilherme Baptista <guisgb13@gmail.com>
2015-03-11 04:17:34 +0100

    All structure in place

--- pelicanconf.py
+++ pelicanconf.py
@@ -5,7 +5,7 @@ from __future__ import unicode_literals
 SITENAME = "XMPP"
 SITEURL = ""
 TIMEZONE = "Europe/Munich"  #Unused (Pelican complains if you don't provide it)
-DIRECT_TEMPLATES = ['index']
+#DIRECT_TEMPLATES = ['index']
 STATIC_PATHS = [ 'img', 'CNAME' ]
 THEME = 'theme'
 MD_EXTENSIONS = [ 'codehilite(css_class=highlight)', 'extra' ]
